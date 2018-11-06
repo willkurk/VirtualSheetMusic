cb213342318624ee3a460a57bfd2bc37f010f58e
William Kurkian <wkurkian@cisco.com>
2018-11-06 16:53:28 -0500

    Working on a javascipt version to use a javascript sheet music renderer

---
+++ jsapp/js/main.js
@@ -0,0 +1,20 @@
+var app = {
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+             $('body').html(new HomeView(self.store).render().el);
+        });
+    }
+
+};
+
+app.initialize();
